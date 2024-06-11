6b31e191613c9008d3ce1e88ffe240515fd92591
Shaf0 <126206716+Shaf0@users.noreply.github.com>
2024-06-11 11:54:09 +1000

    health bar

---
+++ My project (1)/Assets/playerHealth.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class playerHealth : MonoBehaviour
+{
+    public float health; 
+    public float maxHealth; 
+    public Image HealthBar; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
+    }
+}
