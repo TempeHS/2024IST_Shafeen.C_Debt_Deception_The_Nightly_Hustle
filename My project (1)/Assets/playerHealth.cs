0c2fb59c0e04aaf889d74f65083d11b76d8a789c
Shaf0 <126206716+Shaf0@users.noreply.github.com>
2024-07-03 14:13:52 +1000

    Death Screen

--- My project (1)/Assets/playerHealth.cs
+++ My project (1)/Assets/playerHealth.cs
@@ -7,6 +7,9 @@ public class playerHealth : MonoBehaviour
     public float health; 
     public float maxHealth; 
     public Image healthBar; 
+
+    private bool isDead;
+    public GameManagerScript gameManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +20,10 @@ public class playerHealth : MonoBehaviour
     void Update()
     {
         healthBar.fillAmount = Mathf.Clamp(health / maxHealth, 0, 1);
-        if(health <= 0)
+        if(health <= 0 && !isDead)
         {
+            isDead = true;
+            gameManager.gameOver();
             Destroy(gameObject);
         }
     }
