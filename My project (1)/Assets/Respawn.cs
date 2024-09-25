18cf109a8b9cfee29ddb2df127c7eb19be5b0a86
Shaf0 <126206716+Shaf0@users.noreply.github.com>
2024-09-25 14:13:33 +1000

    Finished entire game

--- My project (1)/Assets/Respawn.cs
+++ My project (1)/Assets/Respawn.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class Respawn : MonoBehaviour
 {
     public GameObject player;
-    public Transform respawnpoint;
+   
 
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            player.transform.position = respawnpoint.position;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
